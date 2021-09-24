bbac7f407efbec2595f2e21d0a04486e8f47d493
White <ewhite@usgs.gov>
2021-09-24 11:31:59 -0600

    used appliers for efficient branching and made changes to plots to confirm if correct

--- _targets.R
+++ _targets.R
@@ -1,15 +1,17 @@
-
 library(targets)
 library(tarchetypes)
 library(tibble)
+library(dplyr)
 
 options(tidyverse.quiet = TRUE)
-tar_option_set(packages = c("tidyverse", "dataRetrieval", "urbnmapr", "rnaturalearth", "cowplot"))
+tar_option_set(packages = c("tidyverse", "dataRetrieval", "urbnmapr", "rnaturalearth", "cowplot", "lubridate"))
 
 # Load functions needed by targets below
 source("1_fetch/src/find_oldest_sites.R")
 source("1_fetch/src/get_site_data.R")
 source("3_visualize/src/map_sites.R")
+source("2_process/src/tally_site_obs.R")
+source("3_visualize/src/plot_site_data.R")
 
 # Configuration
 states <- c('WI','MN','MI', 'IL', 'IN', 'IA')
@@ -25,14 +27,16 @@ list(
   # tar_target(mn_data, get_site_data(oldest_active_sites, states[2], parameter)),
   # tar_target(mi_data, get_site_data(oldest_active_sites, states[3], parameter)),
 
-  # Static branching with splitting
+  # Static branching with splitting and appliers
   tar_map(
-    values = tibble(state_abb = states),
+    values = tibble(state_abb = states)%>%
+      mutate(state_plot_files = sprintf("3_visualize/out/timeseries_%s.png", state_abb)),
     tar_target(nwis_inventory, get_state_inventory(sites_info = oldest_active_sites, state_abb)),
-    tar_target(nwis_data, get_site_data(nwis_inventory, state_abb, parameter))
-    # tar_target(count, tally(nwis_data)),
-    # tar_target(fig, plot(count))
-  ),
+    tar_target(nwis_data, get_site_data(nwis_inventory, state_abb, parameter)),
+    tar_target(tally, tally_site_obs(nwis_data)),
+    tar_target(timeseries_png, plot_site_data(state_plot_files, nwis_data, parameter)),
+    names = state_abb
+    ),
 
   # Map oldest sites
   tar_target(
